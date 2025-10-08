4e0190d31f1db874ece22fb6e5e1ce3726ef4da2
andrejabramov <andrzejvod@gmail.com>
2025-10-08 16:14:36 +0300

    currency_bot4

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -1,5 +1,6 @@
+import telebot
 from src.bot.bot_instance import bot
-from src.api.crypto_compare import get_price
+from src.utils.extensions import CryptoConverter, ConvertionException
 from src.models.data import notice, keys, valid_commands
 
 @bot.message_handler(commands=[valid_commands['start'], valid_commands['help']])
@@ -8,30 +9,32 @@ def get_describe(message):
 
 @bot.message_handler(commands=[valid_commands['values']])
 def get_currencies(message):
-    text = 'Доступные валюты:'
+    text = notice['using_currency']
     for key in keys.keys():
         text = '\n'.join((text, key,))
     bot.reply_to(message, text)
-
-@bot.message_handler(commands=['price'])
-def handle_price(message):
-    try:
-        parts = message.text.split()
-        if len(parts) != 3:
-            raise ValueError("Неверный формат. Используй: /price BTC USD")
-        _, from_cur, to_cur = parts
-        price = get_price(from_cur, to_cur)
-        bot.reply_to(message, f"1 {from_cur.upper()} = {price:.2f} {to_cur.upper()}")
-    except Exception as e:
-        bot.reply_to(message, f"Ошибка: {e}")
 
 # Fallback-обработчик для ЛЮБОЙ команды, которая НЕ совпадает с разрешёнными
 @bot.message_handler(func=lambda message: message.text and message.text.startswith('/'))
 def unknown_command(message):
     # Извлекаем команду (без аргументов и без /)
     command = message.text.split()[0][1:].lower()
+    #получаем вывод валидных команд каждая с новой строки
+    valid_cmd = '\n'.join(f'/{cmd}' for cmd in valid_commands.values())
     bot.reply_to(
         message,
-        f"Неизвестная команда: /{command}.\n"
-        f"Доступные команды: /start, /help, /values"
+        f"{notice['unknown']} /{command}.\n"
+        f"{notice['valid_comm']} \n{valid_cmd}"
     )
+
+@bot.message_handler(content_types=['text', ])
+def convert_message(message: telebot.types.Message):
+    try:
+        text = CryptoConverter.valid_data(message.text)
+    except ConvertionException as e:
+        bot.reply_to(message, f'{notice['user_err']}\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'{notice['server_err']}\n{e}')
+    else:
+       bot.send_message(message.chat.id, text)
+
--- src/utils/extensions.py
+++ src/utils/extensions.py
@@ -1,2 +1,43 @@
+import requests
+import json
+from src.models.data import keys, URL, notice
+
+
 class ConvertionException(Exception):
     pass
+
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: float):
+        if quote == base:
+            raise ConvertionException(f'{notice['clon']} {base}.')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'{notice['currency']} {base}.')
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'{notice['currency']} {quote}.')
+        r = requests.get(f'{URL}fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+        return total_base
+
+    @staticmethod
+    def valid_data(input_list: str) -> str:
+        # не указываем разделитель split(), чтобы предупредить ошибк пользователя с двумя и более пробелами или табуляцией
+        values = input_list.split()
+        if len(values) != 3:
+            raise ConvertionException(notice['insert'])
+        quote, base, amount_str = [s.lower() for s in values]
+        try:
+            amount = float(amount_str)
+        except ValueError:
+            raise ConvertionException(f'{notice["amount"]} "{amount_str}" — не число.')
+        if amount <= 0:
+            raise ConvertionException(notice['value_amount'])
+        total_base = CryptoConverter.convert(quote, base, amount)
+        # расчеты стоимости одной валюты относительно другой производим с учетом amount - умножением
+        text = f'Цена {amount} {quote} в {base} - {float(total_base) * float(amount)}'
+        return text
